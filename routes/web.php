c6f79ce13c74259787d3581add911f0b4b612703
nadun <nadhun97@gmail.com>
2024-01-02 09:22:43 +0530

    update project

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Route::get('/', function () {
 
 
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
 
