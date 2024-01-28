780072095739362a7e7a8ecb00d0c1ac2ef4aaa1
nadun <nadhun97@gmail.com>
2024-01-28 14:30:16 +0530

    change file path

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,27 +16,53 @@ class Post extends Model
 
 
 
+
+
     public function scopeFilter($query, array $filters) // Post::newQuery()-> filter()
     {
-        
-        $query->when($filters['search'] ?? false, fn($query, $search) => $query
-                ->where('title', 'like', '%' . $search . '%')
-                ->orwhere('body', 'like', '%' . $search . '%'));
-        
-        
-        
 
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+
+            $query
+
+                ->where('title', 'like', '%' . $search . '%')
+
+                ->orwhere('body', 'like', '%' . $search . '%'));
+
+
+
+        $query->when( $filters['category'] ?? false, fn ($query, $category) =>
             
+            $query->whereHas('category', fn ($query) => 
+            
+                $query -> where('slug', $category))
+
+
+        );
     }
+
+
+
+
 
 
     public function category()
+
     {
+
         return $this->belongsTo(Category::class);
+
     }
 
+
+
     public function author()
+
     {
+
         return $this->belongsTo(User::class, 'user_id');
+
     }
+
+
 }
